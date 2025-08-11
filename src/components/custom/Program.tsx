1948d3bf210bca7980a68a8402235736fdc83eba
Kushal Harsora <kushalharsora13@gmail.com>
2025-08-11 19:09:36 +0530

    Program Card

--- src/components/custom/Program.tsx
+++ src/components/custom/Program.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
-const Program = () => {
+const Card = ({ props }: { props: { title: string, content: string } }) => {
+  return (
+    <React.Fragment>
+      <div>
+        {props.title}
+      </div>
+    </React.Fragment>
+  )
+}
 
-    
+const Program = () => {
 
   return (
     <React.Fragment>
         <section>
-
+          <Card props={{ title: "Test", content: "test" }} />
         </section>
     </React.Fragment>
   )
